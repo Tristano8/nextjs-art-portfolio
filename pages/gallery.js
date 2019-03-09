27955a71e8cdb23ef74c35ea189aa96ea8f806f4
Hugh Davidson <hugh.l.davidson@gmail.com>
2019-03-09 17:58:02 +1100

    Implemented basic image grid

--- pages/gallery.js
+++ pages/gallery.js
@@ -2,16 +2,28 @@ import Layout from '../components/MyLayout'
 import Typography from '@material-ui/core/Typography'
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
+import GridListTileBar from '@material-ui/core/GridListTileBar'
 import { withStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
 
 
 
+
+const imgData = [
+    { title: 'Sunflowers', filename: 'placeholder.png', dimensions: '16 x 12', type: 'portrait', description: 'Some flowers I liked' },
+    { title: 'Sunflowers', filename: 'placeholder.png', dimensions: '16 x 12', type: 'portrait', description: 'Some flowers I liked' },
+    { title: 'Barn with sheep', filename: 'placeholder.png', dimensions: '10 x 8', type: 'landscape', description: 'A painting of some sheep in front of a barn'},
+    { title: 'Still life', filename: 'placeholder.png', dimensions: '20 x 24', type: 'landscape', description: ''},
+]
 
 const styles = theme => ({
     categoryHeading: {
         marginTop: theme.spacing.unit * 2,
         marginLeft: theme.spacing.unit,
+    },
+    gridList: {
+        display: 'flex',
+        width: 600,
+        overflow: 'hidden'
     }
 })
 
@@ -28,37 +40,26 @@ const Gallery = props => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
             incididunt ut labore et dolore magna aliqua.
             </Typography>
-            </div>
-          </div>
-        <div>
-            <Typography className={classes.categoryHeading} gutterBottom={true} variant='h4'>Painting</Typography>
-                <GridList>
-                    <GridListTile >
-                        <img src={`static/artwork/placeholder.png`} alt='dummy' />
-                    </GridListTile>
-                    <GridListTile>
-                        <img src={'static/artwork/placeholder.png'} alt='dummy2'/>
-                    </GridListTile>
+        </div>
+    </div>
+    <div>
+        <Typography className={classes.categoryHeading} gutterBottom={true} variant='h4'>Painting</Typography>
+            <GridList cellHeight='auto' cols='3' spacing='6' className={classes.gridList}>
+                { imgData.map(img => (
+                    <GridListTile cols={img.type === 'landscape' ? 2 : 1}>
+                            <img src={`static/artwork/${img.filename}`} alt='dummy' />
+                        <GridListTileBar
+                            title={`${img.title}`}
+                            subtitle={`${img.dimensions}`}
+                            />
+                        </GridListTile>
+                    ))
+                    }
                 </GridList>
             <Typography className={classes.categoryHeading} gutterBottom={true} variant='h4'>Illustration</Typography>
-                <GridList>
-                <GridListTile >
-                        <img src={`static/artwork/placeholder.png`} alt='dummy' />
-                    </GridListTile>
-                    <GridListTile>
-                        <img src={'static/artwork/placeholder.png'} alt='dummy2'/>
-                    </GridListTile>
-                </GridList>
             <Typography className={classes.categoryHeading} gutterBottom={true} variant='h4'>Design</Typography>
-            <GridList>
-            <GridListTile >
-                        <img src={`static/artwork/placeholder.png`} alt='dummy' />
-                    </GridListTile>
-            <GridListTile>
-                        <img src={'static/artwork/placeholder.png'} alt='dummy2'/>
-                    </GridListTile>
-                    </GridList>
         </div>
+        
     </Layout>
 )
     }
